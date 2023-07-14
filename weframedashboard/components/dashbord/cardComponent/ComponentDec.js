68397e4c0592ccf8bb676815589e718c94e463c3
mon842 <sayakbasak842@gmail.com>
2023-07-14 20:19:11 +0530

    Update ComponentDec.js

--- weframedashboard/components/dashbord/cardComponent/ComponentDec.js
+++ weframedashboard/components/dashbord/cardComponent/ComponentDec.js
@@ -6,14 +6,8 @@ const ComponentDec = (props) => {
     <div className={`w-72 md:w-72 lg:w-52 bg-white rounded-lg my-3 p-2 ${props.border} text-gray-400`}>
         <div className='flex justify-between mt-4  '>
                 <p className='text-xs from-neutral-400'>Space Tasks 2</p>
-                {/* <Image
-                    className='rounded-full'
-                    src={'/base.png'}
-                    height={30}
-                    width={20}
-                    alt='img'
-                /> */}
-                <img className='h-5 w-5 rounded-full' src={'/base.png'} alt='img' />
+
+                <img className='h-5 w-5 rounded-full' src={'/dp4.png'} alt='img' />
             </div>
             <p className='text-xs my-4 text-gray-500 font-bold'>Make Money Online Through</p>
     </div>
