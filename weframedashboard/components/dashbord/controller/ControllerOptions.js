19a54446cd64766cdbba78d8659faa1f6ad08728
mon842 <sayakbasak842@gmail.com>
2023-07-15 20:27:30 +0530

    added color

--- weframedashboard/components/dashbord/controller/ControllerOptions.js
+++ weframedashboard/components/dashbord/controller/ControllerOptions.js
@@ -5,9 +5,9 @@ const ControllerOptions = (props) => {
         <div className={`${props.h} mx-10 lg:mx-3 lg:block`}>
             <div className='flex'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 30 31" fill="none">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d={props.d} fill="#ADB8CC" />
+                    <path fill-rule="evenodd" clip-rule="evenodd" d={props.d} fill={props.fill} />
                 </svg>
-                <p className='md:text-base lg:text-xs mt-3 text-gray-400'>{props.txt}</p>
+                <p className={`md:text-base lg:text-xs font-semibold mt-3 ${props.col}`}>{props.txt}</p>
             </div>
 
 
